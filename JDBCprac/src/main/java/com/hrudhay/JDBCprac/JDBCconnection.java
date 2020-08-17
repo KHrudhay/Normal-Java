ca602519303b856e12132d94887460914d949893
hrudh <hrudh@Hrudhay>
2020-08-17 13:18:32 +0530

    added right mysql jar

--- JDBCprac/src/main/java/com/hrudhay/JDBCprac/JDBCconnection.java
+++ JDBCprac/src/main/java/com/hrudhay/JDBCprac/JDBCconnection.java
@@ -4,10 +4,15 @@ import java.sql.*;
 
 public class JDBCconnection {
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		String url = "jdbc:mysql://localhost:3306/world&useSSL=true", user = "root", password = "Mental1@",
+	public static void main(String[] args) throws SQLException {
+		String url = "jdbc:mysql://localhost:3306/world", user = "root", password = "Mental1@",
 				query = "select * from city where CountryCode='IND'";
-		Class.forName("com.mysql.jdbc.Driver");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Connection con = DriverManager.getConnection(url, user, password);
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(query);
